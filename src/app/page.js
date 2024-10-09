6115cbfe52dcda885d0e50cbaad00dcebb86af12
kyok309 <kyok309xx@gmail.com>
2024-10-09 22:43:24 +0800

    scrollbar change

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,10 @@
-"use client";
-import { useState } from "react";
 import Navbar from "./components/navbar";
 import Section from "./components/section";
 import Section2 from "./components/section2";
-import clsx from "clsx";
 
 export default function Home() {
   return (
-    <main className="min-h-full bg-white antialiased text-center">
+    <main className="min-h-full bg-white antialiased text-center z-0">
       <Navbar/>
       <div className="mt-[44px] tabletMax:mt-[48px] flex flex-col items-center">
         <div className="bg-white flex justify-center flex-wrap text-sm py-3 max-w-[300px] tablet:max-w-[460px] desktop:max-w-fit">
